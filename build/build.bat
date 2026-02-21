236845992102423eefb803cfc9af929fa753beae
adam <adam@flounder.net>
2026-02-20 22:49:55 -0800

    chore: remove Windows batch files, Cygwin bash scripts sufficient

--- build/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-if not exist .venv python -m venv .venv
-.venv\Scripts\pip install -r requirements.txt -r requirements-dev.txt
-echo Setup complete.
--- build/test.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-.venv\Scripts\pytest tests\ -v %*
